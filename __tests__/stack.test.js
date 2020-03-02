35068fe01009deb680eef3ceac93ff4ab60c47c3
romanmashkovskiy <ra.mashkovskiy@gmail.com>
2020-03-02 10:56:32 +0200

    first commit

---
+++ __tests__/stack.test.js
@@ -0,0 +1,23 @@
+import makeStack from '../src/stack.js';
+
+test('stack main flow', () => {
+    const stack = makeStack();
+    stack.push('one');
+    stack.push('two');
+    expect(stack.pop()).toEqual('two');
+    expect(stack.pop()).toEqual('one');
+});
+
+test('isEmpty', () => {
+    const stack = makeStack();
+    expect(stack).toHaveLength(0);
+    stack.push('one');
+    expect(stack).toHaveLength(1);
+    stack.pop();
+    expect(stack).toHaveLength(0);
+});
+
+test('pop empty stack', () => {
+    const stack = makeStack();
+    expect(stack.pop()).toBeUndefined();
+});
